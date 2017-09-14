aafc783f2d8efa8ae7c0afd593d45b0bcc055fdf
mahesh-pardeshi17 <mahesh.pardeshi@metamagic.in>
2017-09-14 12:40:31 +0530

    Microservice Infra services

---
+++ SpringFramework5/config-server/src/main/java/com/metamagic/desire/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.metamagic.desire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Mahesh Pardeshi
+ *
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
